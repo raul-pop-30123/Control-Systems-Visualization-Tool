aa98f48cf996110db5e558c889b71b5af01bf78f
raul-pop-30123 <2005.raul.pop.2005@gmail.com>
2026-08-02 12:07:06 +0300

    feat: Added basic functions to polynom

--- src/math/Polynomial.cpp
+++ src/math/Polynomial.cpp
@@ -2,3 +2,11 @@
 #include <cmath>
 #include <algorithm>
 
+using namespace std;
+
+Polynomial::Polynomial() : coeffs{0.0} {}
+
+Polynomial::Polynomial(vector<double> coeffsAscending) : coeffs(move(coeffsAscending)) {
+    if(coeffs.empty()) coeffs.push_back(0.0);
+    trim();
+}
