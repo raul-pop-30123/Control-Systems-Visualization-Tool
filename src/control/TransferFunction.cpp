5aaad358a6aa53bdb08bfeaf7faa5cedb190d9a6
raul-pop-30123 <2005.raul.pop.2005@gmail.com>
2026-08-03 13:13:47 +0300

    feat: added transfer function implementation

---
+++ src/control/TransferFunction.cpp
@@ -0,0 +1,22 @@
+#include "control/TransferFunction.hpp"
+
+using namespace std;
+
+TransferFunction::TransferFunction(Polynomial numerator, Polynomial denominator) :
+    beta(move(numerator)), alpha(move(denominator)){}
+
+ComplexNumber TransferFunction::evaluate(const ComplexNumber& s) const{
+    return beta.evaluate(s)/alpha.evaluate(s);
+}
+
+vector<ComplexNumber> TransferFunction::poles() const{
+    return alpha.roots();
+}
+
+vector<ComplexNumber> TransferFunction::zeros() const {
+    return beta.roots();
+}
+
+const Polynomial& TransferFunction::numerator() const   { return beta;  }
+const Polynomial& TransferFunction::denominator() const { return alpha; }
+
